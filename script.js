37f4e83809aaa1ab494d5285915e3f4f302fff12
Katarina <hyperv@ucione.local>
2023-01-26 19:41:21 +0100

    App init

---
+++ script.js
@@ -0,0 +1,3 @@
+// git init samo kad radimo projekt iz nule
+// git branch da vidimo ima li dodatnih grana
+// git branch dev -> on pokuša nešto kopirati iz mastera u dev a to se tek može nakon prvog commita
